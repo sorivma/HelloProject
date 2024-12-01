2552ef6c10ceede1b92c9d834b5cbee0514c5869
sorivma <sorivma03.06@gmail.com>
2024-12-01 17:34:12 +0300

    test hook

--- src/main/kotlin/org/example/helloproject/HelloProjectApplication.kt
+++ src/main/kotlin/org/example/helloproject/HelloProjectApplication.kt
@@ -16,5 +16,5 @@ fun main(args: Array<String>) {
 @RestController
 class HelloController {
     @GetMapping("/hello")
-    fun hello() = "Hello World + Test hook"
+    fun hello() = "Hello World + Test hook + test"
 }
