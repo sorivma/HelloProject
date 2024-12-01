835839935c6ed88c636c1155f59686c5157e887f
sorivma <sorivma03.06@gmail.com>
2024-12-01 13:42:21 +0300

    Add something

--- src/main/kotlin/org/example/helloproject/HelloProjectApplication.kt
+++ src/main/kotlin/org/example/helloproject/HelloProjectApplication.kt
@@ -2,10 +2,19 @@ package org.example.helloproject
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class HelloProjectApplication
 
 fun main(args: Array<String>) {
     runApplication<HelloProjectApplication>(*args)
+}
+
+@RestController
+class HelloController {
+    @GetMapping("/hello")
+    fun hello() = "Hello World"
 }
